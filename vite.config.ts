d936b35859d34a43f498483d336303e2df52b639
germanmalykh <germanmalyh@gmail.com>
2025-11-12 20:23:12 +0300

    Fix app path

--- vite.config.ts
+++ vite.config.ts
@@ -21,13 +21,13 @@ export default defineConfig({
         start_url: process.env.NODE_ENV === 'production' ? '/terrorscape/' : '/',
         icons: [
           {
-            src: '/icon-192x192.png',
+            src: process.env.NODE_ENV === 'production' ? '/terrorscape/icon-192x192.png' : '/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable',
           },
           {
-            src: '/icon-512x512.png',
+            src: process.env.NODE_ENV === 'production' ? '/terrorscape/icon-512x512.png' : '/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
